76e413fcf268acd507c8c381c02e31ccc00a97ac
qorvhfl080 <86284854+qorvhfl080@users.noreply.github.com>
2021-09-10 09:57:27 +0900

    데이터바인딩

--- app/src/main/java/com/nepplus/finalproject_20210910/MainActivity.kt
+++ app/src/main/java/com/nepplus/finalproject_20210910/MainActivity.kt
@@ -2,6 +2,7 @@ package com.nepplus.finalproject_20210910
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
 import com.nepplus.finalproject_20210910.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -10,8 +11,8 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
+        //setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
         binding.textTxt.text = "abcdefg"
     }
 
